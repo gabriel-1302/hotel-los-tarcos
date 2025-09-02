e7e17b4f98ba56e95a823aaf434d43029815bb90
gabriel-1302 <gabriel.velasquez.1290@gmail.com>
2025-09-02 11:19:57 -0400

    urls cuartos y rutas, 3 de ejemplo

--- hotel/core/views.py
+++ hotel/core/views.py
@@ -19,6 +19,15 @@ def galeria(request):
 def paquetes(request):
     return render(request, 'core/paquetes.html')
 
+def habitacion_simple(request):
+    return render(request, 'core/habitacion_simple.html')
+
+def habitacion_normal(request):
+    return render(request, 'core/habitacion_normal.html')
+
+def habitacion_matrimonial(request):
+    return render(request, 'core/habitacion_matrimonial.html')
+
 
 # def home_en(request):
 #     return render(request, 'core/base_en.html')
