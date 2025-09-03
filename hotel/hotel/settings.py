536bef15312891fc727bedb100a33a963b349ad6
gabriel-1302 <gabriel.velasquez.1290@gmail.com>
2025-09-03 01:18:26 -0400

    nada

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!b*#&w=c$r9!b=)@d^#*h&z@q@q*z(w*q@q*z(w*q@q*z(w*q@q*z('
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
